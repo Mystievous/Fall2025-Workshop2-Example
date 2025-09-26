9ab9020a12ebf027e215a121df321d54b994785a
Mystievous <mystievous@starseekstudios.com>
2025-09-25 17:46:00 -0700

    comment main menu script

--- levels/main_menu/main_menu.gd
+++ levels/main_menu/main_menu.gd
@@ -1,6 +1,10 @@
 extends Control
 
+# The scene we want the button to go to
 @export var level_start: PackedScene
 
+# Through the `Node` tab, the `StartButton` clicked signal is assigned to this function
+# If you click the green icon on the left of the function you can also see all the connected signals.
 func start_game():
+	# Unload the current scene and switch to the specified one
 	get_tree().change_scene_to_packed(level_start)
