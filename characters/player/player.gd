fd46a3cf30cb3c08fb731e67a2548612b36adf28
Mystievous <mystievous@starseekstudios.com>
2025-09-26 13:36:09 -0700

    add more details in comments

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -19,6 +19,10 @@ signal health_changed
 
 func _physics_process(delta: float) -> void:
 	# Gets the left/right movement
+	# 
+	# These "ui_{direction}" inputs are set by default, but you can
+	# also make new ones by going to Project > Project Settings > Input Map.
+	# Then, just reference them the same way by name.
 	var x_input = Input.get_axis("ui_left", "ui_right")
 	
 	# The `velocity` field is from the `CharacterBody2D`.
@@ -37,8 +41,18 @@ func _physics_process(delta: float) -> void:
 	# interacting with any collision bodies along the way.
 	move_and_slide()
 	
+# Check if the ground raycast is colliding with the ground
 func isOnGround():
-	# Check if the ground raycast is colliding with the ground
+	# The raycast is referenced here with a "unique name"
+	# You can do this by right clicking a node and hitting "Access as Unique Name".
+	# Then, you can either manually type the name with a percentage sign in front, or
+	# drag and drop the node directly into your code.
+	#
+	# This works best in self-contained scenes like this, where you always know exactly
+	# what nodes you use.
+	# 
+	# However, you might want to avoid this for the most part, as if you ever change
+	# the name of the node, you must also change every reference to the name independently.
 	return %GroundRayCast.is_colliding()
 
 func try_jump():
@@ -60,5 +74,6 @@ func damage(amount: int):
 	%Sprite2D.modulate = Color(0.707, 0.124, 0.0)
 	%HurtColorTimeout.start()
 
+# Signal connected through the tab to the right *from* the HurtColorTimeout node.
 func _on_hurt_color_timeout() -> void:
 	%Sprite2D.modulate = Color(1, 1, 1)
